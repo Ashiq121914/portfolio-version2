719d5bd5f1382cfe325d88d49f42d513e8c1e495
Ashiq121914 <ashiq121914@gmail.com>
2024-12-14 20:49:59 +0600

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // Components
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Review from "./components/Review";
@@ -19,6 +20,7 @@ const App = () => {
         <Review />
         <Contact />
       </main>
+      <Footer />
     </>
   );
 };
