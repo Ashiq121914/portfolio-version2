70f626f2bd24e5193c359091c7edb9f9d7e86558
Ashiq121914 <ashiq121914@gmail.com>
2024-12-14 22:12:52 +0600

    bug fig

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="/public/images/avatar-1.jpg"
+                src=" images/avatar-1.jpg"
                 alt="Gazi Ashiq portrait"
                 width={40}
                 height={40}
@@ -46,7 +46,7 @@ const Hero = () => {
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-70% rounded-[60px] overflow-hidden">
             <img
-              src="/public/images/hero-banner2.png"
+              src=" images/hero-banner2.png"
               width={456}
               height={600}
               alt="Gazi Ashiq"
